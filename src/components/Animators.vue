04e08aed1c5d2f785921bccfffd072610ad471bb
German <georg.88@mail.ru>
2019-02-16 16:31:32 +0700

    add environment variables

---
+++ src/components/Animators.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li v-for="animator in animators" :key="animator.name">
+      {{ animator.name }}
+    </li>
+  </ul>
+</template>
+
+<script>
+import { environment } from '../environment'
+
+export default {
+  name: 'Animators',
+  data() {
+    return {
+      animators: []
+    }
+  },
+  mounted() {
+    const api = environment.api;
+    fetch(`${api}/animators`).then(res => {
+      res.json().then(data => this.animators = data);
+    })
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  background: silver;
+}
+</style>
---
+++ src/environment.js
@@ -0,0 +1,3 @@
+export const environment = {
+  api: process.env.VUE_APP_API
+}
